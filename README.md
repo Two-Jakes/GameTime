d6b76f00688f9e828e9498fe54ce42430f3d99fd
Jake Bertish <bertishprofessional@gmail.com>
2023-08-25 13:56:40 -0500

    Initial Commit Setup Project

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # GameTime
 Dashboard of simple games made by two Jakes.
+
+# Expo Router Application
---
+++ app/_layout.js
@@ -0,0 +1,17 @@
+import { Stack } from 'expo-router';
+
+export default function Layout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#f4511e',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}
+    />
+  );
+}
