021f9ae318bf93aa221b40bea66695dd12a0be4e
Jake Bertish <bertishprofessional@gmail.com>
2023-08-28 14:02:43 -0500

    Update Styles in Tic Tac Toe

--- components/games/tic-tac-toe/TicTacToe.jsx
+++ components/games/tic-tac-toe/TicTacToe.jsx
@@ -4,73 +4,71 @@ import styles from './tictactoe.style'
 
 const TicTacToe = () => {
   let initialBoard = [
-    {id: 0, value: ''},
-    {id: 1, value: ''}, 
-    {id: 2, value: ''},
-    {id: 3, value: ''},
-    {id: 4, value: ''},
-    {id: 5, value: ''},
-    {id: 6, value: ''},
-    {id: 7, value: ''},
-    {id: 8, value: ''}
+    { id: 0, value: 'X' },
+    { id: 1, value: 'X' },
+    { id: 2, value: 'X' },
+    { id: 3, value: 'O' },
+    { id: 4, value: 'O' },
+    { id: 5, value: 'O' },
+    { id: 6, value: 'X' },
+    { id: 7, value: 'X' },
+    { id: 8, value: 'X' }
   ];
-  
+
   const [board, updateBoard] = useState(initialBoard);
 
   //HELPER FUNCTIONS
 
   handlePress = (item) => {
     console.log("Item", item, board[0].value);
-    if (board[item].value === ''){
-      updateBoard([{value: "X"}]);
-    } else if (board[item].value === 'X'){
-      updateBoard([{value: "O"}]);
-    }
+    //updateBoard([{id: item, value: "X"}]);
   }
 
   return (
     <View>
-      <Text style={styles.toeText}>Welcome to Tic Tac Toe</Text>
+      <View style={styles.container}>
+        <Text style={styles.toeText}>Tic Tac Toe</Text>
+      </View>
 
       {/* -----------------------------ROW ONE------------------------------------ */}
       <View style={styles.boardSpaceContainer}>
-          <TouchableOpacity style={styles.boardSpace} onPress={() => this.handlePress(0)}>
-              <Text style={styles.toeText}>{board[0].value}</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.boardSpace} onPress={() => console.log(1,2)}>
-          <Text style={styles.toeText}>1,2</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.boardSpace} onPress={() => console.log(1,3)}>
-          <Text style={styles.toeText}>1,3</Text>
-          </TouchableOpacity>
+        <TouchableOpacity style={styles.boardSpace} onPress={() => this.handlePress(0)}>
+          <Text style={styles.boardText}>{board[0].value}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.boardSpace} onPress={() => this.handlePress(1)}>
+          <Text style={styles.boardText}>{board[1].value}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.boardSpace} onPress={() => console.log(1, 3)}>
+          <Text style={styles.boardText}>{board[2].value}</Text>
+        </TouchableOpacity>
       </View>
       {/* -----------------------------ROW ONE------------------------------------ */}
 
       {/* -----------------------------ROW TWO------------------------------------ */}
       <View style={styles.boardSpaceContainer}>
-          <TouchableOpacity style={styles.boardSpace} onPress={() => console.log(2,1)}>
-              <Text style={styles.toeText}>2,1</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.boardSpace} onPress={() => console.log(2,2)}>
-          <Text style={styles.toeText}>2,2</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.boardSpace} onPress={() => console.log(2,3)}>
-          <Text style={styles.toeText}>2,3</Text>
-          </TouchableOpacity>
+        <TouchableOpacity style={styles.boardSpace} onPress={() => console.log(2, 1)}>
+          <Text style={styles.boardText}>{board[3].value}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.boardSpace} onPress={() => console.log(2, 2)}>
+          <Text style={styles.boardText}>{board[4].value}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.boardSpace} onPress={() => console.log(2, 3)}>
+          <Text style={styles.boardText}>{board[5].value}</Text>
+        </TouchableOpacity>
       </View>
       {/* -----------------------------ROW TWO------------------------------------ */}
 
       {/* -----------------------------ROW THREE------------------------------------ */}
       <View style={styles.boardSpaceContainer}>
-          <TouchableOpacity style={styles.boardSpace} onPress={() => console.log(3,1)}>
-              <Text style={styles.toeText}>3,1</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.boardSpace} onPress={() => console.log(3,2)}>
-          <Text style={styles.toeText}>3,2</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.boardSpace} onPress={() => console.log(3,3)}>
-          <Text style={styles.toeText}>3,3</Text>
-          </TouchableOpacity>
+        <TouchableOpacity style={styles.boardSpace} onPress={() => console.log(3, 1)}>
+          <Text style={styles.boardText}>{board[6].value}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.boardSpace} onPress={() => console.log(3, 2)}>
+          <Text style={styles.boardText}>{board[7].value}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.boardSpace} onPress={() => console.log(3, 3)}>
+          <Text style={styles.boardText}>{board[8].value}</Text>
+        </TouchableOpacity>
       </View>
       {/* -----------------------------ROW THREE------------------------------------ */}
     </View>
--- components/games/tic-tac-toe/tictactoe.style.js
+++ components/games/tic-tac-toe/tictactoe.style.js
@@ -3,20 +3,37 @@ import { COLORS, SIZES, FONT } from "../../../constants";
 
 const styles = StyleSheet.create({
   container: {
+    width: '100%',
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+    marginTop: 50,
   },
   toeText: {
-    fontSize: SIZES.medium,
+    fontFamily: FONT.bold,
+    fontSize: SIZES.xLarge,
+    color: COLORS.white,
+    marginTop: 30,
+    marginBottom: 30,
+    paddingBottom: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+  },
+  boardText: {
+    fontSize: SIZES.xxLarge,
     color: COLORS.white,
     fontFamily: FONT.bold,
+    paddingLeft: 7
   },
   boardSpaceContainer: {
     flexDirection: 'row',
     justifyContent: 'center',
   },
   boardSpace: {
-    padding: 10,
-    length: 50,
-    width: 50,
+    padding: 30,
+    length: 100,
+    width: 100,
     justifyContent: 'space-between',
     borderColor: 'white',
     borderWidth: StyleSheet.hairlineWidth,
